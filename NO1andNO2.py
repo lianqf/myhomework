7755d75939c0bfb3934a11278b9ef072bb30608c
lianqf <lianqf@sohu.com>
2015-11-04 12:24:37 +0800

    update 1.01

--- NO1andNO2.py
+++ NO1andNO2.py
@@ -1,11 +1,12 @@
-list = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
+list_mun = [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
 
 
 NO1 = 0
 NO2 = 0
 
-for temp in list:
+for temp in list_mun:
     if temp > NO1:
+        NO2 = NO1
         NO1 = temp
         continue
     elif temp > NO2 :
